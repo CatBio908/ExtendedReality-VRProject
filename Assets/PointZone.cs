9ec8e869a300dad260b22ebc6cc8205e45d83181
CatBio908 <162396898+CatBio908@users.noreply.github.com>
2025-10-23 13:10:51 -0500

    Don't Be (fire) Alarmed

--- Assets/PointZone.cs
+++ Assets/PointZone.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class PointZone : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int ScoreAmount = 12;
     private void OnTriggerEnter(Collider other)
     {
-        ScoreManager.instance.AddScore(11);
+        ScoreManager.instance.AddScore(ScoreAmount);
         BasketBallSpawner.instance.DestroyBall(other.gameObject); 
         Destroy(other.gameObject);
     }
